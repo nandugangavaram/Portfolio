d6de73e4a63e8ee808c9cbbd2bf0fecf16206e8f
nandugangavaram <nandugangavaram@gmail.com>
2024-03-25 22:58:08 +1100

    Updated Work place logos

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -15,7 +15,7 @@ const Works = () => {
 					<div className="works-body">
 						<div className="work">
 							<img
-								src="Portfolio/facebook.png"
+								src="./Portfolio/facebook.png"
 								alt="facebook"
 								className="work-image"
 							/>
@@ -30,7 +30,7 @@ const Works = () => {
 
 						<div className="work">
 							<img
-								src="Portfolio/twitter.png"
+								src="./Portfolio/infor_logo.png"
 								alt="twitter"
 								className="work-image"
 							/>
@@ -47,7 +47,7 @@ const Works = () => {
 
 						<div className="work">
 							<img
-								src="Portfolio/twitter.png"
+								src="Portfolio/hexagon_logo.png"
 								alt="twitter"
 								className="work-image"
 							/>
